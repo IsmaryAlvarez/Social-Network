ea1458537f95acb5f3135647e9cc3772eb7b3f98
kuveee <kamilavillablanca23@gmail.com>
2018-01-04 12:22:32 -0300

    actualizando funcionalidad boton

--- assets/js/app.js
+++ assets/js/app.js
@@ -178,30 +178,26 @@ document.getElementById('btn-google').addEventListener('click',
         provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
         
         console.log("Launching modal");
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user);
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log("Firebase error > "+errorMessage)
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-            if (errorCode === 'auth/account-exists-with-different-credential'){
-              alert ('Es el mismo usuario');
-            }
-          });
-        }else{
-          console.log("Holo");
-        }
+
+ firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
       });
-}
+}};
 
 
