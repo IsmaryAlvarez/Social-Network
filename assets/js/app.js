6f62942b701ef2a90af3ba0378f517f533622097
kuveee <kamilavillablanca23@gmail.com>
2018-01-04 12:03:06 -0300

    Firebase test

--- assets/js/app.js
+++ assets/js/app.js
@@ -169,40 +169,40 @@ $(".container-foods").hide();
 
 
 
-/*
+window.onload = function(){
+document.getElementById('btn-google').addEventListener('click',
+  function(){
+    console.log("Boton de google presionado!")
+    if (!firebase.auth().currentUser){
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        
+        console.log("Launching modal");
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("Firebase error > "+errorMessage)
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+            if (errorCode === 'auth/account-exists-with-different-credential'){
+              alert ('Es el mismo usuario');
+            }
+          });
+        }else{
+          console.log("Holo");
+          firebase.auth().singOut();
+        }
+      });
+}
 
 
-document.getElementById('btn-google').addEventListener('click',function(){
-if (!firebase.auth().currentUser){
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  console.log(user);
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-      if (errorCode === 'auth/account-exists-with-different-credential'){
-        alert ('Es el mismo usuario');
-      }
-    });
-  }else{
-    firebase.auth().singOut();
-  }
-});
-
-
-
-
-
-*/
